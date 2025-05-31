037e457762edd935c81ef15b244c8a2c47bf559e
MateusSM2000 <matt_petao@hotmail.com>
2025-05-31 01:46:48 -0300

    update

--- 199.py
+++ 199.py
@@ -1,6 +1,8 @@
 #o init faz com q, no momento em q uma instancia da classe é criada, a função __init__ é automaticamente executada
 #os códigos dentro da classe q n são métodos ou funções, são executados apenas uma vez logo quando roda o programa
-#os atributos da classe tbm se tornam atributos das instâncias
+#as variaveis declaradas dentro da classe mas fora de metodos ou funcoes (atributos da classe) e os atributos do objeto sao declaradas no escopo global
+#atributos da classe sao acessados com 'classe.variavel' e tbm se tornam atributos das instâncias,
+# mas elas NAO sao conectadas entre si apesar do atributo do objeto receber o msm valor quando criado. se um mudar o outro n muda
 
 class Pessoa:
     print('a')
@@ -8,11 +10,22 @@ class Pessoa:
     def __init__(self, nome, sobrenome):
         self.nome = nome
         self.sobrenome = sobrenome
+        print(self.especie)
 
 p1 = Pessoa('Mateus','Schmidt Mesquita')
 p2 = Pessoa('Gabriele','Borges')
+print()
 
 print(p1.nome,p1.sobrenome)
 print(p2.nome,p2.sobrenome)
 print(p1.especie)
 print(p2.especie)
+print()
+
+print(Pessoa.especie)
+p1.especie = 'humano'
+print(p1.especie)
+print(Pessoa.especie)
+Pessoa.especie = 'humano'
+print(Pessoa.especie)
+p3 = Pessoa('Marcos','Batista')
